d21ae7ef7c8c40e4312a5978684cf39920b30089
davider3 <dreinhardt389@gmail.com>
2022-10-24 20:21:55 -0600

    Added Practice Teensy Project

---
+++ TeensyPractice/TeensyPractice.ino
@@ -0,0 +1,20 @@
+/*
+ * Created By: David Reinhardt
+ * 24 Oct 2022
+ * 
+ * For: Electronics SubTeam BYU Racing learning how to use the Teensy in data acquisition 
+ * and CANBus Communication
+ * 
+ */
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
