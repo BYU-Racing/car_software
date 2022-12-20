f9cc41ea5a47fe59a08b396f0488c5af4f5a2426
davider3 <dreinhardt389@gmail.com>
2022-12-19 17:28:28 -0700

    This program successfully blinks an LED and writes to the Serial Monitor

--- TeensyWithVSCode/src/main.cpp
+++ TeensyWithVSCode/src/main.cpp
@@ -1,9 +1,30 @@
+/*
+ * Created by David Reinhardt
+ * First Code to be uploaded to one of our Teensys
+ * Goal: Blink an LED and write to the serial Monitor
+ */
+
 #include <Arduino.h>
 
+#define LED 2
+
 void setup() {
-  // put your setup code here, to run once:
+  //SETUP THE LED PIN
+  pinMode(LED, OUTPUT);
+
+  //TURNON SERIAL MONITOR
+  Serial.begin(9600);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //TURN ON THE LED FOR HALF A SECOND AND WRITE ON TO SERIAL MONITOR
+  digitalWrite(LED, HIGH);
+  Serial.println("On");
+  delay(500);
+
+  //TURN OFF THE LED FOR HALF A SECOND AND WRITE OFF TO SERIAL MONITOR
+  digitalWrite(LED, LOW);
+  Serial.println("Off");
+  delay(500);
 }
