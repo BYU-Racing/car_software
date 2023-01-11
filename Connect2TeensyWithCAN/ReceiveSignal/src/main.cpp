fa362c5dbdf32f1bab8c4905b7741d7146873037
davider3 <dreinhardt389@gmail.com>
2023-01-10 19:44:43 -0700

    Worked to figure out the FlexCan_T4 library

--- Connect2TeensyWithCAN/ReceiveSignal/src/main.cpp
+++ Connect2TeensyWithCAN/ReceiveSignal/src/main.cpp
@@ -5,23 +5,24 @@
  * Goal: Receive a Signal over CAN and turn on an LED
  */
 #include <Arduino.h>
-#include <FlexCAN.h>
+#include <FlexCAN_T4.h>
 
 #define LED 2
-//TODO: DEFINE CANBUS PINS
+FlexCAN_T4<CAN3, RX_SIZE_256, TX_SIZE_16> receiver;
 
 void setup() {
   //SETUP LED PIN
   pinMode(LED, OUTPUT);
 
   //TODO: SETUP CANBUS PINS
-
 }
 
 void loop() {
+
   if(0){ //TODO: MAKE THIS IF STATEMENT DEPEND ON SIGNALS COMING FROM THE CANBUS
     digitalWrite(LED, HIGH);
   }else{
     digitalWrite(LED, LOW);
   }
+
 }
