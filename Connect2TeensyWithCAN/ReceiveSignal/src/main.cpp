74fad3abd6be7e960f40efc334fe9b62432f3094
Bridger Danby <bridger.danby@outlook.com>
2023-01-23 17:52:58 -0700

    did a few things to receive

--- Connect2TeensyWithCAN/ReceiveSignal/src/main.cpp
+++ Connect2TeensyWithCAN/ReceiveSignal/src/main.cpp
@@ -10,15 +10,22 @@
 #define LED 2
 FlexCAN_T4<CAN3, RX_SIZE_256, TX_SIZE_16> receiver;
 
+void canSniff(const CAN_message_t &msg) {
+
+}
+
 void setup() {
   //SETUP LED PIN
   pinMode(LED, OUTPUT);
 
   //TODO: SETUP CANBUS PINS
+  Serial.begin(1152000); delay(400);
+  receiver.begin();
+  receiver.setClock(CLK_60MHz);
+  receiver.onReceive(canSniff);
 }
 
 void loop() {
-
   if(0){ //TODO: MAKE THIS IF STATEMENT DEPEND ON SIGNALS COMING FROM THE CANBUS
     digitalWrite(LED, HIGH);
   }else{
