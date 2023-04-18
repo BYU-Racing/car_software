d61c8cae425eb7ffd4b7182033f3c7546e3b04af
davider3 <dreinhardt389@gmail.com>
2023-04-18 10:35:50 -0600

    Improved Professionalism

--- ExpoDisplay/Motor/src/main.cpp
+++ ExpoDisplay/Motor/src/main.cpp
@@ -1,64 +1,44 @@
+/*
+ * BYU RACING
+ * Created By David Reinhardt
+ * Goal: Receive signals from the CANBus to control
+ *       speed of a DC Motor
+ */
 #include <Arduino.h>
 #include <FlexCAN_T4.h>
-
-
-const int ledPin = 13;
 
 // DEFINE GLOBAL CONSTANTS
 #define DC1 6
 #define DC2 5
 #define DCPWM 7
-
 CAN_message_t rmsg;
+FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16> can2;
 
 // FUNCTION PROTOTYPES
 void spinMotor(int);
-void stop();
-
-FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> myCan1;
-FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16> myCan2;
-FlexCAN_T4<CAN3, RX_SIZE_256, TX_SIZE_16> myCan3;
 
 void setup() {
-  // put your setup code here, to run once:
+  // SET UP SERIAL MONITOR
   Serial.begin(9600);
-  pinMode(ledPin, OUTPUT);
-  
-myCan1.begin();
-myCan2.begin();
-myCan3.begin();
-myCan1.setBaudRate(250*1000);
-myCan2.setBaudRate(250*1000);
-myCan3.setBaudRate(250*1000);
 
+  // SET UP CAN
+  can2.begin();
+  can2.setBaudRate(250*1000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-CAN_message_t msg;
-msg.len=1;
-msg.buf[0]=254;
 
-msg.id=2;
-myCan2.write(msg);
+  // IF A CAN MESSAGE IS RECEIVED ASSIGN IT TO rmsg
+  if ( can2.read(rmsg) ) {
+    Serial.println(rmsg.buf[0]);
+    spinMotor(rmsg.buf[0]);
+  }
 
- if ( myCan2.read(rmsg) ) 
- {
-  Serial.println(rmsg.buf[0]);
-  spinMotor(rmsg.buf[0]);
- }
-
-//digitalWrite(ledPin, !digitalRead(ledPin));
-//delay (500);
 }
 
-
+// SPINS THE MOTOR AT THE PROVIDED SPEED
 void spinMotor(int speed){
-    digitalWrite(DC1, HIGH);
-    digitalWrite(DC2, LOW);
-    analogWrite(DCPWM, speed);
-}
-
-void stop(){
-    digitalWrite(DC1, LOW);
+  digitalWrite(DC1, HIGH);
+  digitalWrite(DC2, LOW);
+  analogWrite(DCPWM, speed);
 }
--- ExpoDisplay/Throttle/src/main.cpp
+++ ExpoDisplay/Throttle/src/main.cpp
@@ -1,43 +1,59 @@
+/*
+ * BYU RACING
+ * Created By David Reinhardt
+ * Goal: Send signals containing throttle position 
+ *       over the CANBus
+ */
 #include <Arduino.h>
 #include <FlexCAN_T4.h>
 
+// GLOBAL VARIABLES
 #define POT A0
+FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16> can2;
+
+// CALIBRATION CONSTANTS
 const float BIAS = 200;
 const float RANGE = 800 - BIAS;
 
-FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16> can1;
-
+// FUNCTION PROTOTYPES
 int speedCalc(int);
 
 void setup() {
+  // SET UP SERIAL MONITOR
   Serial.begin(9600);
   Serial.println("Start");
 
   // SET UP CAN
-  can1.begin();
-  can1.setBaudRate(250000);
+  can2.begin();
+  can2.setBaudRate(250000);
 }
 
 void loop() {
   
-
+  // CREATE AND SEND THE CAN MESSAGE
   CAN_message_t msg;
-  msg.len=1;
-  msg.id=1;
-  msg.buf[0]=speedCalc(analogRead(POT));
+  msg.len=1; // SETS MESSAGE LENGTH
+  msg.buf[0]=speedCalc(analogRead(POT)); // SEND SPEED VALUE
   msg.id=2;
-  can1.write(msg);
+  can2.write(msg);
 
 }
 
+// FUNCTION TO CONVERT THROTTLE POSITION TO PWM VALUE FOR DC MOTOR
 int speedCalc(int pot){
+  // IF VALUE OF POTENTIOMETER IS BELOW OUR TESTES
+  // VALUE FOR ZERO RETURN 0
   if(pot < BIAS) return 0;
 
+  // CALCULATE  THROTTLE PRESSED PERCENTAGE
   float percent = (pot - BIAS) / RANGE;
 
+  // USE VALUES FOUND FOR DC MOTOR TO GET 
+  // PWM VALUE BASED ON THROTTLE POSITION
   float speed = percent * 109 + 145;
 
+  // IF VALUE EXCEEDS MAX PWM VALUE RETURN 254
   if(speed > 254) return 254;
-  else return (int)speed;
 
+  else return (int)speed;
 }
