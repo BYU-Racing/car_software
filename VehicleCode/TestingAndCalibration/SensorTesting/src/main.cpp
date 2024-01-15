71ad69ba4ae892f3bbf465bddeb9a1ba21a4e460
davider3 <dreinhardt389@gmail.com>
2024-01-15 11:47:32 -0700

    Progress on redundant throttle

--- VehicleCode/TestingAndCalibration/SensorTesting/src/main.cpp
+++ VehicleCode/TestingAndCalibration/SensorTesting/src/main.cpp
@@ -4,7 +4,10 @@
 #include "Sensor.h"
 #include "SensorData.h"
 
-#define POT 24
+#define POT1 24
+#define POT2 25
+#define ID1 1
+#define ID2 2
 const float BIAS = 200;
 const float RANGE = 800 - BIAS;
 
@@ -13,16 +16,16 @@ FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16> can1;
 
 // int speedCalc(int);
 
-int percentCalc1(double pot, double bias=0.);
+int percentCalc(double, double, double);
+int* buildData(int, int);
 int getLow(int percent);
 int getHigh(int percent);
 int speedCalc(int pot);
 
 // initialize throttle sensor
-int throttleID = 1;
 int throttleFreq = 10;
-int throttlePin = POT;
-AnalogSensor throttle = AnalogSensor(throttleID, throttleFreq, throttlePin);
+AnalogSensor throttle1 = AnalogSensor(ID1, throttleFreq, POT1);
+AnalogSensor throttle2 = AnalogSensor(ID2, throttleFreq, POT2);
 
 
 void setup() {
@@ -36,14 +39,34 @@ void setup() {
 }
 
 void loop() {
-  int percent = 0;
+  int percent1 = 0;
+  int percent2 = 0;
   double bias = 0;
   const int torque = 200;
-  if (throttle.readyToCheck()) {
+  if (throttle1.readyToCheck() && throttle2.readyToCheck()) {
     // read throttle sensor
-    percent = percentCalc1(throttle.readInputs(), bias);
-    Serial.println(percent/100);
+    // percent1 = percentCalc(throttle1.readInputs(), bias, 1024);
+    percent2 = percentCalc(throttle2.readInputs(), 93, 962);
+    // Serial.print("Throttle 1: ");
+    // Serial.println(percent1);
+    Serial.print("Throttle 2: ");
+    Serial.println(percent2);
 
+    // if(abs(percent1 - percent2) < 25){
+      int length = 8;
+
+      int* sendData = buildData(torque, percent2);
+
+      // send CAN message
+      SensorData message = SensorData(ID1, sendData, length, millis());
+      // message.toString();
+      can1.write(message.formatCAN());
+    // }
+  }
+  delay(1000);
+}
+
+int* buildData(int torque, int percent){
     // convert to motor controller format
     int torqueLow = getLow(torque);
     int torqueHigh = getHigh(torque);
@@ -62,17 +85,11 @@ void loop() {
     sendData[6] = 0;
     sendData[7] = 0;
 
-    // send CAN message
-    SensorData message = SensorData(throttleID, sendData, length, millis());
-    message.toString();
-    can1.write(message.formatCAN());
-  }
-  delay(200);
+    return sendData;
 }
 
-int percentCalc1(double pot, double bias) {
+int percentCalc(double pot, double bias, double max) {
   Serial.println(pot);
-  double max = 1024;
   double weight = 10000 / max;
   double percent = (pot + bias) * weight;
   return static_cast<int>(percent);
