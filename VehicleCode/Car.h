ce810346d7d81b71198f5081ced4ed83b86c0582
CZandt <95643051+CZandt@users.noreply.github.com>
2024-05-04 17:16:25 -0600

    Added Changes to the Car Class

--- VehicleCode/Car.h
+++ VehicleCode/Car.h
@@ -1,41 +1,64 @@
 #ifndef CAR_H
 #define CAR_H
 
-#include "SensorData.h"  // Include the header for the SensorData class
-#include <string>        // Include the header for string handling
+#include "SensorData.h"
+#include <string>
 #include <SD.h> 
+
+// teensy hardware info
+#define GO_PIN 23
+#define ACCELERATOR_POT_1 3
+
+// commands and logging integers
+#define ID_ERROR 0
+#define SHUTDOWN 1
+#define CAR_ON 0
+#define NO_COMMAND 2
+#define COMMAND_IDX 1
+#define START_STOP_ID 22
+
+// global unchanging magic numbers
+#define SCALE 100
+#define START_THRESHOLD 10
+#define BYTE_VALUE 256
+#define MAX_NAME_LENGTH 6
+#define MOTOR_SIGNAL_DELAY 1500
+
 
 
 class Car {
 private:
     // Car states
-    bool active;              // Indicates if the car is active
-    bool key;                 // Indicates if the car has been turned on with the key
-    bool switchOn;            // Indicates if the car has been switched on
-    bool logState;            // Indicates if the car is logging data
-    int timeZero;             // Starting time
-    int saveDelay = 10000;    // Delay for saving data
-    unsigned long lastSave;   // Time of the last save
+    bool goFast = false;                // Indicates if the motor should spin
+    bool prevGoState = false;           // Previous state of the go switch
+    unsigned long lastGoUpdate;         // Time of the last update
+    unsigned long goUpdateSpeed = 500;  // Speed of the go switch update
 
+    bool logState = false;              // Indicates if the car is logging data
+    bool prevLogState = false;          // Previous state of the log switch
+    unsigned long lastLogUpdate;        // Time of the last update
+    unsigned long logUpdateSpeed = 500; // Speed of the log switch update
+    unsigned long lastOffSignal;
+    unsigned long repeatOffDelay = 500;// Delay for repeating the off command
+    
     // Logging attributes
-    String fileName;          // File name for logging data
-    File dataFile;            // File for logging data
-    FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16> can2;
-
-    // Other attributes
-    int buttonState;          // State of the button (not used rn)
-    int prevButtonState = 0;
-    const int scale = 100;
-    const int startThreshold = 10;
-    const int byteValue = 256;
-    const int maxNameLength = 6;
+    int timeZero;                       // Starting time
+    unsigned long saveDelay = 20000;    // Delay for saving data
+    unsigned long lastSave;             // Time of the last save
+    String fileName;                    // File name for logging data
+    File dataFile;                      // File for logging data
+    FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16> myCan;
 
     // Global variables
     int speed;
     int count = 0;
     int i = 0;
+    int j = 0;
     int* sensorData;
+    int fileCount = 0;
     CAN_message_t rmsg;
+    CAN_message_t startMotor; 
+    CAN_message_t stopMotor;
 
     // Helper methods for logging data and SD card
     void logData(SensorData*);
@@ -45,12 +68,9 @@ private:
     void shutdown();
 
     // Hardware checking
-    void updateState();
-    void checkKey();
-    void checkButton();
-    void checkSwitch();
+    void updateState(bool newState);
     void checkToLog();
-    void buttonPushed();
+    void countFiles();
 
     // Helper methods for setting up the SD card
     String updateFileName();
@@ -62,18 +82,17 @@ private:
 public:
     // Constructors and Destructors
     Car();
-    Car(String logFileName, FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16> can2);
+    Car(String logFileName, FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16> myCan);
     ~Car();
+    void initialize(FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16>, int);
 
     // Setters
-    void setActive(bool);
-    void setLogState(bool);
+    void refreshLog();
     void setSaveDelay(int);
+    void setLogState(bool);
     void resetTimeZero(unsigned long);
-    void setCAN(FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16> can2);
+    void setCAN(FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16> myCan);
 
-    // Getters
-    bool checkActive();
     int deconstructSpeed(int*); // not used rn
 
     // Method to save data
